543f7d4b6e89bcf60f1fb41e671a688849257719
sumitsgram <beingsumit01@gmail.com>
2023-12-17 18:27:07 +0530

    Sign Up Api

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req,res) => {
+export const signup = async (req,res,next) => {
 const {username ,email ,password} = req.body;
 const hashedPassword = bcryptjs.hashSync(password,11);
 const newUser = new User ({username , email, password:hashedPassword });
@@ -9,5 +9,5 @@ try {
     await newUser.save();
     res.status(201).json({message:"User created successfully"});
 } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
 }};
